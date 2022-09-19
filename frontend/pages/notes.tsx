f25def643df984a473cf1344db787f4633f64d33
Geek-o-Byte <mmiwf16@gmail.com>
2022-09-19 18:20:20 +0300

    ASS-4

--- frontend/pages/notes.tsx
+++ frontend/pages/notes.tsx
@@ -26,6 +26,7 @@ const Notes = () => {
                 <li>Рекуррентные нейронные сети</li>
                 <li></li>Применение методов машинного и глубокого обучения для решения задач компьютерного зрения и интеллектуального анализа текстов
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est nostrum ipsam illo, reprehenderit aspernatur ut odit similique voluptatem repudiandae architecto ipsa sapiente tempora quo optio assumenda. Nesciunt sunt dolorum corrupti rem nemo veritatis officia natus iusto, id officiis deserunt nobis et molestiae accusantium dolores expedita non est ea. Enim, consectetur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus ab, illum eaque eius, obcaecati autem neque dignissimos voluptate quas veniam animi esse asperiores explicabo repellat quos. Sint odio, dolores quos dicta molestias culpa, dolorum consequatur esse vitae laborum maiores! Ut placeat optio hic voluptatem ad inventore quos doloremque cum ratione tempore perferendis eaque, sit rem nobis debitis explicabo ipsam id laudantium illo reprehenderit accusamus! Quas iusto, ducimus fuga autem eius, dicta exercitationem aliquam, officia impedit voluptatibus accusantium distinctio nemo magnam labore expedita sint aperiam explicabo. Deleniti fugit minus itaque. Ex cupiditate eius blanditiis accusamus optio voluptas quae quis ab!
                 </ul>
             </span>
         </PageTemplate>
